0b0312431a67ba9c32698f2e756ad9cccee75031
Yitaek Hwang <yitaek@gmail.com>
2015-11-12 20:50:28 -0500

    added config file to run

--- plot_two_level.m
+++ plot_two_level.m
@@ -17,6 +17,7 @@ MPKI = C{7}./(C{8}/1000); % misses per thousand instructions
 MPKI_bar = reshape(MPKI, 15, [])';
 config = categories(categorical(strcat(C{2}, num2str(C{5}))));
 
+%% 2D Representation of All Configurations
 figure(1)
 bar(MPKI_bar)
 set(gca, 'XTickLabel', benchmarks)
@@ -25,7 +26,7 @@ xlabel('Benchmarks')
 title('MPKI per Benchmark for All Predictor Type')
 legend(config, 'Location', 'Best')
 
-%% Determining the Effects of Width
+%% 3D Representation of All Configurations
 figure(2)
 bar3(MPKI_bar)
 zlabel('MPKI')
@@ -34,3 +35,21 @@ xlabel('Configurations')
 
 set(gca, 'YTickLabel', benchmarks)
 set(gca, 'XTickLabel', config)
+
+%% Comparing Schemes with Same Length
+figure(3)
+colors = ['b', 'm', 'c', 'r', 'g', 'b'];
+syms = ['o', 'x', 's', '+', 'd', '*', 'v', 'p'];
+MPKI_matrix = reshape(MPKI, 5, 3, 5);
+for i=1:5
+    for j=1:3
+        col_index = randi(6,1);
+        sym_index = randi(8,1);
+        colsym = [colors(col_index), syms(sym_index)];
+        scatter(width(1:5), MPKI_matrix(:,j,i), colsym)
+        hold on
+    end
+end
+xlabel('Width')
+ylabel('MPKI')
+hold off
