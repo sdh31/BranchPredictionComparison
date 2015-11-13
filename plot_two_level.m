89484dad3f55785f804aec689b83b89c9527c4d7
Yitaek Hwang <yitaek@gmail.com>
2015-11-13 01:52:56 -0500

    added gshare graphs

--- plot_two_level.m
+++ plot_two_level.m
@@ -8,51 +8,86 @@ fid = fopen('TwoLevelData.csv');
 C = textscan(fid, '%s %s %f %f %f %f %f %f', 'Delimiter', ',', 'EmptyValue', -Inf);
 fclose(fid);
 
+% swapping columns for easier sorting
+% cellHolder = C{6};
+% C{6} = C{8};
+% C{8} = cellHolder;
+
 benchmarks = categories(categorical(C{1})); % name of benchmarks
 type = categories(categorical(C{2})); % type of predictor
 width = C{5};
 MPKI = C{7}./(C{8}/1000); % misses per thousand instructions
 
+%[sortA, sortI] = sort(C{8});
+% for i=1:7
+%     sort(C{i}
+%sortedC = C(sortI, :);
+
 %% Displaying Data
-MPKI_bar = reshape(MPKI, 15, [])';
+MPKI_bar = reshape(MPKI(1:2:150), 15, [])';
+MPKI_bar_xor = reshape(MPKI(2:2:150), 15, [])';
 config = categories(categorical(strcat(C{2}, num2str(C{5}))));
 
-%% 2D Representation of All Configurations
+%% 2D Representation of All Configurations - 2 Level 
 figure(1)
 bar(MPKI_bar)
 set(gca, 'XTickLabel', benchmarks)
 legend(config, 'Location', 'BestOutside')
 ylabel('MPKI')
 xlabel('Benchmarks')
-title('MPKI per Benchmark for All Predictor Type')
+title('MPKI per Benchmark for All Predictor Type - 2 Level')
 
 print('2d_2lev_benchmark', '-djpeg')
 
-%% 3D Representation of All Configurations
+%% 3D Representation of All Configurations - 2 Level
 figure(2)
 bar3(MPKI_bar)
 zlabel('MPKI')
 ylabel('Benchmark')
 xlabel('Configurations')
-
+title('3D Representation - 2 Level')
 set(gca, 'YTickLabel', benchmarks)
 set(gca, 'XTickLabel', config)
+
 print('3d_2lev_benchmark', '-djpeg')
 
-%% Comparing Schemes with Same Length
+%% 2D Representation of All Configurations - gshare 
 figure(3)
-colors = ['b', 'm', 'c', 'r', 'g', 'b'];
-syms = ['o', 'x', 's', '+', 'd', '*', 'v', 'p'];
-MPKI_matrix = reshape(MPKI, 5, 3, 5);
-for i=1:5
-    for j=1:3
-        col_index = randi(6,1);
-        sym_index = randi(8,1);
-        colsym = [colors(col_index), syms(sym_index)];
-        scatter(width(1:5), MPKI_matrix(:,j,i), colsym)
-        hold on
-    end
-end
-xlabel('Width')
+bar(MPKI_bar_xor)
+set(gca, 'XTickLabel', benchmarks)
+legend(config, 'Location', 'BestOutside')
 ylabel('MPKI')
-hold off
+xlabel('Benchmarks')
+title('MPKI per Benchmark for All Predictor Type - gshare')
+
+print('2d_gshare_benchmark', '-djpeg')
+
+%% 3D Representation of All Configurations - gshare
+figure(4)
+bar3(MPKI_bar_xor)
+zlabel('MPKI')
+ylabel('Benchmark')
+xlabel('Configurations')
+title('3D Representation - gshare')
+set(gca, 'YTickLabel', benchmarks)
+set(gca, 'XTickLabel', config)
+
+print('3d_gshare_benchmark', '-djpeg')
+
+% %% Comparing Schemes with Same Length
+% figure(3)
+% colors = ['b', 'm', 'c', 'r', 'g', 'b'];
+% syms = ['o', 'x', 's', '+', 'd', '*', 'v', 'p'];
+% MPKI_matrix = reshape(MPKI, 5, 3, 5);
+% for i=1:5
+%     for j=1:3
+%         col_index = randi(6,1);
+%         sym_index = randi(8,1);
+%         colsym = [colors(col_index), syms(sym_index)];
+%         scatter(width(1:5), MPKI_matrix(:,j,i), colsym)
+%         hold on
+%     end
+% end
+% xlabel('Width')
+% ylabel('MPKI')
+% hold off
