5cfdefc4615560577d8da07489e04dc5d122fecf
Yitaek Hwang <yitaek@gmail.com>
2015-12-01 18:31:09 -0500

    gshare hardware costs

--- plot_two_level_PAp.m
+++ plot_two_level_PAp.m
@@ -10,29 +10,28 @@ fclose(fid);
 
 benchmarks = categories(categorical(C{1})); % name of benchmarks
 width = C{5};
-MPKI = C{7}./(C{8}/1000); % misses per thousand instructions
-
+percentage = C{7}.*100./C{8}; % misses per thousand instructions
 
 %% Displaying Data
-MPKI_bar = reshape(MPKI(1:2:end), 6, [])';
-MPKI_bar_xor = reshape(MPKI(2:2:end), 6, [])';
+percentage_bar = reshape(percentage(1:2:end), 6, [])';
+percentage_bar_xor = reshape(percentage(2:2:end), 6, [])';
 config = categories(categorical(strcat(C{2}, num2str(C{5}))));
 
 %% 2D Representation of PAp Configuration - 2 Level 
 figure(1)
-bar(MPKI_bar)
+bar(percentage_bar)
 set(gca, 'XTickLabel', benchmarks)
 legend(config, 'Location', 'BestOutside')
-ylabel('MPKI')
+ylabel('Percent Mispredicted')
 xlabel('Benchmarks')
-title('MPKI per Benchmark for PAp Configuration - 2 Level')
+title('Percent Mispredicted per Benchmark for PAp Configuration - 2 Level')
  
 print('2d_2lev_PAp_benchmark', '-djpeg')
 
 %% 3D Representation of PAp Configurations - 2 Level
 figure(2)
-bar3(MPKI_bar)
-zlabel('MPKI')
+bar3(percentage_bar)
+zlabel('Percent Mispredicted')
 ylabel('Benchmark')
 xlabel('Configurations')
 title('3D Representation PAp Configuration - 2 Level')
@@ -43,19 +42,19 @@ print('3d_2lev_PAp_benchmark', '-djpeg')
 
 %% 2D Representation of PAp Configurations - gshare 
 figure(3)
-bar(MPKI_bar_xor)
+bar(percentage_bar_xor)
 set(gca, 'XTickLabel', benchmarks)
 legend(config, 'Location', 'BestOutside')
-ylabel('MPKI')
+ylabel('Percent Mispredicted')
 xlabel('Benchmarks')
-title('MPKI per Benchmark for PAp Configuration - gshare')
+title('Percent Mispredicted per Benchmark for PAp Configuration - gshare')
 
 print('2d_gshare_PAp_benchmark', '-djpeg')
 
 %% 3D Representation of All Configurations - gshare
 figure(4)
-bar3(MPKI_bar_xor)
-zlabel('MPKI')
+bar3(percentage_bar_xor)
+zlabel('Percent Mispredicted')
 ylabel('Benchmark')
 xlabel('Configurations')
 title('3D Representation PAp Configuration - gshare')
